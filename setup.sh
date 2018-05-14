c2140ef1521942d6cc999f92f8f2b2d8b350f6b4
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-05-14 09:17:25 +0900

    v946p1_2 init

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ export SKFlat_LIB_PATH=$SKFlat_WD/lib/
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$SKFlat_WD/include/:$SKFlat_WD/src/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SKFlat_LIB_PATH
 
-export SKFlatV="v944_3"
-export SKFlatAnV="v944_3__Default"
+export SKFlatV="v946p1_2"
+export SKFlatAnV="v946p1_2__Default"
 export DATA_DIR=$SKFlat_WD/data/$SKFlatAnV
 mkdir -p $DATA_DIR
 export SKFlatRunlogDir="/data7/Users/jskim/SKFlatRunlog/"
