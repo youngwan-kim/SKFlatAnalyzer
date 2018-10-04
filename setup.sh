172eb7ee2b9b851fc6952940a5a11e113d4f51c5
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-10-04 19:03:30 +0900

    use shared root for snu

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ mkdir -p $SKFlat_WD/data/$SKFlatV
 
 export SAMPLE_DATA_DIR=$SKFlat_WD/data/$SKFlatV/Sample/
 export SKFlatSEDir="/xrootd/store/user/$USER/SKFlatOutput/"
-export SKFlatLogEmail=''
+export SKFlatLogEmail='jskim@cern.ch'
 
 if [[ $HOSTNAME == *"ui10.sdfarm.kr"* ]]; then
   echo "Working on KISTI"
@@ -27,18 +27,16 @@ if [[ $HOSTNAME == *"ui10.sdfarm.kr"* ]]; then
   source /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_9_cand2/external/slc6_amd64_gcc630/bin/thisroot.sh
 
 else
+
   echo "Working on 42cluster"
   export SKFlatRunlogDir="/data7/Users/$USER/SKFlatRunlog/"
   export SKFlatOutputDir="/data7/Users/$USER/SKFlatOutput/"
   export DATA_DIR=$SKFlat_WD/data/$SKFlatV
 
-  export CMS_PATH=/cvmfs/cms.cern.ch
-  source $CMS_PATH/cmsset_default.sh
-  export SCRAM_ARCH=slc6_amd64_gcc630
-  cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_9_cand2/src/
-  eval `scramv1 runtime -sh`
-  cd -
-  source /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_9_cand2/external/slc6_amd64_gcc630/bin/thisroot.sh
+  source /share/apps/root_v6-12-06/bin/thisroot.sh
+  export GCC_HOME=/share/apps/gcc491
+  export PATH=$GCC_HOME/bin:$PATH
+  export LD_LIBRARY_PATH=$GCC_HOME/lib64:$GCC_HOME/lib:$LD_LIBRARY_PATH
 
 fi
 export MYBIN=$SKFlat_WD/bin/
