f1a798c366a0e1d09e8cd374ab4668af6b1af3d1
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-12-06 12:05:48 +0900

    Mu8 norm updated with new DYxsec

--- DataFormats/src/Event.C
+++ DataFormats/src/Event.C
@@ -96,8 +96,9 @@ double Event::GetTriggerLumi(TString trig){
     }
     else if(trig=="HLT_Mu8_v"){
       //==== 180829 Norm Checked : sf = 1.41036
-      //return 2.605*1.41036;
-      return 2.605*1.41036;
+      //==== 181206 Norm Checked : sf = 1.33461
+      //return 2.605;;
+      return 2.605*1.33461;
     }
     else if(trig=="HLT_Mu17_v"){
       return 70.039;
