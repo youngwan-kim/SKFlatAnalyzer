0f9bb42f5482a67dbfb2e1c860eb3ee5ae4bc7c6
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-02-06 08:00:10 +0900

    Add AnalyzerTools directory to include path

--- setup_tmp.sh
+++ setup_tmp.sh
@@ -65,7 +65,7 @@ export MYBIN=$SKFlat_WD/bin/
 export PYTHONDIR=$SKFlat_WD/python/
 export PATH=${MYBIN}:${PYTHONDIR}:${PATH}
 
-export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$SKFlat_WD/DataFormats/include/:$SKFlat_WD/Analyzers/include/
+export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$SKFlat_WD/DataFormats/include/:$SKFlat_WD/AnalyzerTools/include/:$SKFlat_WD/Analyzers/include/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SKFlat_LIB_PATH
 
 source $SKFlat_WD/bin/BashColorSets.sh
