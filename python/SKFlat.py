f7dd3270172cb6b47efbd10372b6ccb884c49315
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-03-26 01:21:44 +0900

    Add EGamma data stream for 2018

--- python/SKFlat.py
+++ python/SKFlat.py
@@ -107,7 +107,7 @@ if IsKNU:
 
 ## Make Sample List
 
-InputSample_Data = ["DoubleMuon", "DoubleEG", "SingleMuon", "SingleElectron", "SinglePhoton", "MuonEG"]
+InputSample_Data = ["DoubleMuon", "DoubleEG", "SingleMuon", "SingleElectron", "SinglePhoton", "MuonEG", "EGamma"]
 AvailableDataPeriods = []
 if args.Year == "2016":
   AvailableDataPeriods = ["B_ver2","C","D","E","F","G","H"]
