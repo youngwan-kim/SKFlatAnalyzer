b28454e5722ea3fa9b3c973d45bcfe6dc99e82e7
sansan9401 <sansan9401@gmail.com>
2019-05-19 20:32:46 +0900

    Faster GetEffLumi

--- Analyzers/src/GetEffLumi.C
+++ Analyzers/src/GetEffLumi.C
@@ -1,12 +1,19 @@
 #include "GetEffLumi.h"
 
+void GetEffLumi::initializeAnalyzer(){
+  fChain->SetBranchStatus("*",0);
+  fChain->SetBranchStatus("gen_weight",1);
+  fChain->SetBranchStatus("PDFWeights_Scale",1);  
+}
+
 void GetEffLumi::executeEvent(){
 
-  Event ev = GetEvent();
+  double MCweight = 1.;
+  if(!IsDATA) MCweight = gen_weight>0 ? 1. : -1. ;
 
-  FillHist("sumW", 0, ev.MCweight(), 1, 0., 1.);
+  FillHist("sumW", 0, MCweight, 1, 0., 1.);
   if(!IsDATA && PDFWeights_Scale->size()>0){
-    FillHist("sumW_Reweight", 0, ev.MCweight()*PDFWeights_Scale->at(0), 1, 0., 1.);
+    FillHist("sumW_Reweight", 0, MCweight*PDFWeights_Scale->at(0), 1, 0., 1.);
   }
 
 }
