b16529aba8f4b92c821832dbbaf0c3f4ee840b2a
Jae Sung Kim <jae.sung.kim@cern.ch>
2020-01-07 15:03:20 +0900

    add script summarizing sample info

---
+++ data/Run2Legacy_v4/2016/Sample/Summarize.py
@@ -0,0 +1,27 @@
+import os
+
+os.system('ls -1 CommonSampleInfo/*.txt &> temp.txt');
+lines = open('temp.txt').readlines()
+os.system('rm temp.txt')
+
+out_MC = open('SampleSummary_MC.txt','w')
+out_Sig_HNWR = open('SampleSummary_Signal_HNWR.txt','w')
+
+FirstLine = '# alias PD xsec nmc sumw\n'
+out_MC.write(FirstLine)
+out_Sig_HNWR.write(FirstLine)
+
+for line in lines:
+
+  line = line.strip('\n')
+  alias = line.replace('CommonSampleInfo/','').replace('.txt','')
+
+  infoline = open(line).readlines()[1]
+
+  if alias.startswith("WRtoNLtoLLJJ_"):
+    out_Sig_HNWR.write(infoline)
+  else:
+    out_MC.write(infoline)
+
+out_MC.close()
+out_Sig_HNWR.close()
