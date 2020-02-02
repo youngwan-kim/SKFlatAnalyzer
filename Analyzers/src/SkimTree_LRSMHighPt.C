ba207e58baae13bc4009a5b9e60c93742a204149
Jae Sung Kim <jae.sung.kim@cern.ch>
2020-02-02 20:49:56 +0900

    save gen_weight for all MC; 2016 LRSM sample filelist updated

--- Analyzers/src/SkimTree_LRSMHighPt.C
+++ Analyzers/src/SkimTree_LRSMHighPt.C
@@ -14,6 +14,7 @@ void SkimTree_LRSMHighPt::initializeAnalyzer(){
     if(!IsDATA){
       newtree->SetBranchStatus("gen_*",0);
       newtree->SetBranchStatus("LHE_*",0);
+      newtree->SetBranchStatus("gen_weight",1); // for MCweight()
     }
   }
 
