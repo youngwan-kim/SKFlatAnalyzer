f1ce82dfa467b14f9b8c879c5d0a75e5ce59fdff
Youngwan Kim <youngwan.kim@cern.ch>
2022-09-07 11:58:36 +0900

    EXO-22-011 FO lepton selection bug fix

--- Analyzers/src/HNL_TriLep.C
+++ Analyzers/src/HNL_TriLep.C
@@ -137,13 +137,15 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
 
   // All Leptons
   AllLeptons.clear();
-  AllLeptons = CombineLeptonPointerVector(AllMuons,AllElectrons);
+  AllLeptons = CombineLeptonPointerVector(AllElectrons,AllMuons);
+  if(HasFlag("debug")) cout << "[DEBUG] AllLeptons" << endl;
 
   // Muon Selection
   vector<Muon> this_AllMuons = AllMuons;
   vector<Muon> muons = SelectMuons(this_AllMuons, param.Muon_Tight_ID, 10., 2.4) ;
   vector<Muon> muons_FO = SelectMuons(this_AllMuons, param.Muon_Loose_ID, 10., 2.4) ;
   vector<Muon> muons_loose = SelectMuons(this_AllMuons, param.Muon_Loose_ID, 5., 2.4) ;
+  if(HasFlag("debug")) cout << "[DEBUG] MuonSelection" << endl;
 
   // Remove Electrons having Loose Muon within dR<0.05
   for(unsigned int i=0 ; i<AllElectrons.size(); i++){
@@ -154,6 +156,7 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
       }
     }
   }
+  if(HasFlag("debug")) cout << "[DEBUG] LooseMuonCleaning" << endl;
 
   // Electron Selection
   vector<Electron> this_AllElectrons = AllElectrons;
@@ -162,34 +165,51 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
   vector<Electron> electrons_loose = SelectElectrons(this_AllElectrons, param.Electron_Loose_ID, 5., 2.5);
   
   // Fakeable Object Selection
+
+  double MVASlope(0); double MVAIntercept(0);
+  if(DataYear==2016){
+    MVASlope = -.00025; MVAIntercept = .025;
+  }
+  if(DataYear==2017 || DataYear==2018){
+    MVASlope = -.0005; MVAIntercept = .035;
+  }
+
+/*vector<Muon> muons_FO;
+  for(unsigned int i=0; i<AllMuons.size(); i++){
+    if(!(AllMuons.at(i).PassID(param.Muon_Loose_ID))) continue;
+    if(!(AllMuons.at(i).Pt()>10. && fabs(AllMuons.at(i).Eta())<2.4)) continue;
+    Jet closestJet = GetClosestJet(AllJets,muons_FO.at(i));
+    if(AllMuons.at(i).MVA()>0.4) continue;
+    double CutFinal = MVASlope*AllMuons.at(i).Pt()+MVAIntercept;
+    muons_FO.push_back(AllMuons.at(i));
+  }*/
+
   for(unsigned int i=0; i<muons_FO.size(); i++){
     Jet closestJet = GetClosestJet(AllJets,muons_FO.at(i));
-    if(!( muons_FO.at(i).MVA()>0.4 )){
+    if(muons_FO.at(i).MVA()>0.4){
       muons_FO.erase(muons_FO.begin()+i);
+      continue;
     }
     else{
-      double CutFinal(0);
-      if(DataYear==2016){
-        CutFinal = -.00025*muons_FO.at(i).Pt()+.025;
-      }
-      if(DataYear==2017 || DataYear==2018){
-        CutFinal = -.0005*muons_FO.at(i).Pt()+.035;
-      }
-      if(!( electrons_FO.at(i).Pt()/closestJet.Pt()>0.45 && closestJet.GetTaggerResult(JetTagging::DeepCSV) < CutFinal)){
+      double CutFinal = MVASlope*muons_FO.at(i).Pt()+MVAIntercept;
+      if(!( muons_FO.at(i).Pt()/closestJet.Pt()>0.45 && closestJet.GetTaggerResult(JetTagging::DeepCSV) < CutFinal)){
         muons_FO.erase(muons_FO.begin()+i);
       }
     }
   }
+  if(HasFlag("debug")) cout << "[DEBUG] MuonFOSelection" << endl;
 
   for(unsigned int i=0; i<electrons_FO.size(); i++){
     Jet closestJet = GetClosestJet(AllJets,electrons_FO.at(i));
-    if(!( electrons_FO.at(i).MVAIso()>0.4 )){
+    if(electrons_FO.at(i).MVAIso()>0.4){
       electrons_FO.erase(electrons_FO.begin()+i);
+      continue;
     }
     else{
       if(!( electrons_FO.at(i).Pt()/closestJet.Pt()>0.45 && closestJet.GetTaggerResult(JetTagging::DeepCSV)>0.5 )) electrons_FO.erase(electrons_FO.begin()+i);
     }
   }
+  if(HasFlag("debug")) cout << "[DEBUG] ElectronFOSelection" << endl;
 
   // Loose Lepton Clean AllTaus/AllJets
   vector<Lepton *> LooseLeps = CombineLeptonPointerVector(electrons_loose,muons_loose);
@@ -201,6 +221,7 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
       }
     }
   }
+  if(HasFlag("debug")) cout << "[DEBUG] TauLooseLepClean" << endl;
 
   // Select b-jets before loose lepton cleaning 
   // FO Lepton cleaning + 1a method reweighting
@@ -214,10 +235,12 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
       }
     }
   }
+  if(HasFlag("debug")) cout << "[DEBUG] BJetFOLepClean" << endl;
 
   JetTagging::Parameters jtps = JetTagging::Parameters(JetTagging::DeepJet, JetTagging::Loose, JetTagging::incl, JetTagging::mujets);
   vector<Jet> jets_tmp = SelectJets(AllJets_tmp, param.Jet_ID, 25., 2.4) ;
   vector<Jet> BJets = GetBJets(jets_tmp,jtps);
+  if(HasFlag("debug")) cout << "[DEBUG] BJetSelection" << endl;
 
   // Loose Lepton Clean AllJets
   for(unsigned int i=0; i<AllJets.size(); i++){
@@ -228,6 +251,7 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
       }
     }
   }
+  if(HasFlag("debug")) cout << "[DEBUG] JetsLooseLepClean" << endl;
 
   // Tau & Jet Collection
   vector<Tau> this_AllTaus = AllTaus;
@@ -240,27 +264,33 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
   vector<Lepton *> TightLeptons = CombineLeptonPointerVector(electrons,muons);
   vector<Lepton *> FOLeptons = CombineLeptonPointerVector(electrons_FO,muons_FO);
   std::sort(TightLeptons.begin(),TightLeptons.end(),PtComparingPtr);
+  if(HasFlag("debug")) cout << "[DEBUG] TightLepton" << endl;
 
   // Baseline Selection
   // 1. 3 tight leptons
   if(!( TightLeptons.size() == 3)) return;
   FillHist("CutFlow",1.,weight,0.,10.,10);
+  if(HasFlag("debug")) cout << "[DEBUG] TightTriLepCut" << endl;
 
   // 2. 4th FO lepton veto
   if(FOLeptons.size() == 4) return;
   FillHist("CutFlow",2.,weight,0.,10.,10);
+  if(HasFlag("debug")) cout << "[DEBUG] FourthFOVeto" << endl;
 
   // 3. same sign veto
   if(TightLeptons.at(0)->Charge() == TightLeptons.at(1)->Charge() && TightLeptons.at(1)->Charge() == TightLeptons.at(2)->Charge()) return;
   FillHist("CutFlow",3.,weight,0.,10.,10);
+  if(HasFlag("debug")) cout << "[DEBUG] SameSignVeto" << endl;
 
   // 4. b-jet veto
   if(BJets.size()>0) return;
   FillHist("CutFlow",4.,weight,0.,10.,10);
+  if(HasFlag("debug")) cout << "[DEBUG] BJetVeto" << endl;
 
   // 5. pT cut
   if(!( TightLeptons.at(0)->Pt() > 15 && TightLeptons.at(1)->Pt() > 10 && TightLeptons.at(2)->Pt() > 10 )) return;
   FillHist("CutFlow",5.,weight,0.,10.,10);
+  if(HasFlag("debug")) cout << "[DEBUG] pTCut" << endl;
 
   // 6. OSSF mZ 15GeV window cut (for all OSSF pairs) + mOSSF > 5
   for(unsigned int i=0; i<TightLeptons.size(); i++){
@@ -277,6 +307,7 @@ void HNL_TriLep::executeEventFromParameter(AnalyzerParameter param){
     }
   }
   FillHist("CutFlow",6.,weight,0.,10.,10);
+  if(HasFlag("debug")) cout << "[DEBUG] mOSSF" << endl;
 
 }
 
